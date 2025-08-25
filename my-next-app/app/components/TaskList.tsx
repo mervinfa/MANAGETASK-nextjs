6f74837db1d1c9cfd611d371fed191f6c57be288
mervin fauzhan atkly <merpin4060@gmail.com>
2025-08-25 10:59:40 +0700

    change text

--- my-next-app/app/components/TaskList.tsx
+++ my-next-app/app/components/TaskList.tsx
@@ -8,7 +8,7 @@ export default function TaskList({tasks}:TaskListProps){
         <ul className="text-[#37353E] mt-4 px-4 list-disc list-inside">
              {tasks.length === 0 && <p className="text-[#37353E]">Belum ada task</p>}
             {tasks.map((task,i)=>(
-                <li key={i} className="text-[#37353E] p-2 border-b ">
+                <li key={i} className="text-[#37353E] p-2 font-bold text-xl">
                     {task}
                 </li>
             ))}
